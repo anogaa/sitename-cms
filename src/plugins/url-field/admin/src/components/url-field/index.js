ff5faf6607c3388d83f00cdc41ff1dc36c23f95a
arjun <arjun.solankia703@gmail.com>
2023-03-10 17:47:07 +0530

    regex changes

--- src/plugins/url-field/admin/src/components/url-field/index.js
+++ src/plugins/url-field/admin/src/components/url-field/index.js
@@ -18,7 +18,7 @@ export default function Index({
 
   useEffect(() => {
     var res = prompt.match(
-      /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
+      /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/
     );
     if (res == null) {
       return (
