1603e68e85a3b376482cc7097806f543c2115ab2
arjun <arjun.solankia703@gmail.com>
2023-03-14 17:18:23 +0530

    watchlist add in scorecard

---
+++ src/api/scorecard-subcategory/controllers/scorecard-subcategory.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * scorecard-subcategory controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::scorecard-subcategory.scorecard-subcategory');
---
+++ src/api/scorecard-subcategory/services/scorecard-subcategory.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * scorecard-subcategory service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::scorecard-subcategory.scorecard-subcategory');
